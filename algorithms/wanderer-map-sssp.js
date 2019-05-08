e99b80ef8175d06e9ee9e752d075f1c87a83bb1c
kondfox <peter.tamas@greenfoxacademy.com>
2019-05-08 23:20:28 +0200

    refactor(wanderer-map-sssp.js): simplify findNeighbours function and reorganize function orders

--- algorithms/wanderer-map-sssp.js
+++ algorithms/wanderer-map-sssp.js
@@ -1,26 +1,30 @@
 'use strict';
 
 const findShortestPath = (map, from, to) => {
-  from = node(...from);
-  to = node(...to);
-
   const edgeList = generateEdgeList(map);
-  const previousNodes = bfs(edgeList, from, to);
-  const shortestPath = reconstractPath(previousNodes, from, to);
-
+  const previousNodes = bfs(edgeList, node(...from), node(...to));
+  const shortestPath = reconstractPath(previousNodes, node(...from), node(...to));
   return shortestPath;
 }
 
-const reconstractPath = (previousNodes, from, to) => {
-  let path = [];
-  let actual = to;
-
-  while (actual !== from) {
-    path.unshift(actual);
-    actual = previousNodes[actual];
+const generateEdgeList = (map) => {
+  let edgeList = {};
+  for (let i = 0; i < map.length; i++) {
+    for (let j = 0; j < map[i].length; j++) {
+      edgeList[node(i, j)] = findNeighbours(i, j, map);
+    }
   }
+  return edgeList;
+}
 
-  return path;
+const node = (x, y) => {
+  return `[${x}, ${y}]`;
+}
+
+const findNeighbours = (x, y, map) => {
+  return [[-1, 0], [1, 0], [0, -1], [0, 1]]
+    .map(i => (map[x + i[0]] && map[x + i[0]][y + i[1]]) ? node(x + i[0], y + i[1]) : null)
+    .filter(c => c);
 }
 
 const bfs = (edgeList, from, to) => {
@@ -40,37 +44,19 @@ const bfs = (edgeList, from, to) => {
 }
 
 const initializePreviousNodes = (edgeList) => {
-  return Object.keys(edgeList)
-          .reduce((prev, node) => {
-            prev[node] = null;
-            return prev;
-          }, {});
+  return Object.keys(edgeList).reduce((prev, node) => { prev[node] = null; return prev; }, {});
 }
 
-const generateEdgeList = (map) => {
-  let edgeList = {};
-  for (let i = 0; i < map.length; i++) {
-    for (let j = 0; j < map[i].length; j++) {
-      edgeList[node(i, j)] = findNeighbours(i, j, map);
-    }
+const reconstractPath = (previousNodes, from, to) => {
+  let path = [];
+  let actual = to;
+
+  while (actual !== from) {
+    path.unshift(actual);
+    actual = previousNodes[actual];
   }
-  return edgeList;
-}
 
-const node = (x, y) => {
-  return `[${x}, ${y}]`;
-}
-
-const findNeighbours = (x, y, map) => {
-  let neighbours = [];
-  for (let i = -1; i <= 1; i++) {
-    for (let j = -1; j <= 1; j++) {
-      if ((Math.abs(i) ^ Math.abs(j)) && map[x + i] && map[x + i][y + j]) {
-        neighbours.push(node(x + i, y + j));
-      }
-    }
-  }
-  return neighbours;
+  return path;
 }
 
 const map = [
@@ -86,5 +72,8 @@ const map = [
   [1, 1, 1, 0, 1, 0, 0, 1, 1, 1]
 ];
 
-const sp = findShortestPath(map, [0, 0], [3, 7]);
-console.log(sp);
+console.log(findNeighbours(1, 1, map));
+
+
+// const sp = findShortestPath(map, [0, 0], [3, 7]);
+// console.log(sp);
